35f79f4da788fb8259ebad6ce10b54f74597cea1
fightwing <2608379678@qq.com>
2020-09-25 14:10:30 +0800

    footer finished

---
+++ back-end/src/main/java/com/twuc/shopping/po/Product.java
@@ -0,0 +1,8 @@
+package com.twuc.shopping.po;
+
+/**
+ * @author Boyu Yuan
+ * @date 2020/9/25 14:09
+ */
+public class Product {
+}
---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = () => <p>TW Mall @2020 Created by Boyu Yuan</p>;
+
+export default Footer;
