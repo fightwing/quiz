29bc781abf32d7e26584d1b0a024b212cf29f51a
fightwing <2608379678@qq.com>
2020-09-25 15:21:27 +0800

    database finished

--- back-end/src/main/java/com/twuc/shopping/po/Order.java
+++ back-end/src/main/java/com/twuc/shopping/po/Order.java
@@ -1,8 +1,31 @@
 package com.twuc.shopping.po;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Boyu Yuan
  * @date 2020/9/25 14:35
  */
+@Table(name = "order_list")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
 public class Order {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int price;
+    private int number;
+    private String unit;
 }
--- back-end/src/main/java/com/twuc/shopping/po/Product.java
+++ back-end/src/main/java/com/twuc/shopping/po/Product.java
@@ -1,8 +1,32 @@
 package com.twuc.shopping.po;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Boyu Yuan
  * @date 2020/9/25 14:09
  */
+@Table(name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
 public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int price;
+    private String unit;
+    private String url;
+
 }
